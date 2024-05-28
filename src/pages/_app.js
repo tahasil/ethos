97777384e0e2719c73e0d386e01f6800e6d7688d
tahasil <tahasil@aiolos.solutions>
2024-05-28 18:02:24 +0530

    Cursor done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,12 +3,39 @@ import "bootstrap/dist/css/bootstrap.min.css";
 //import "../app/globals.css";
 import "../app/master.css";
 
-import AnimatedCursor from "../component/cursor1";
+//import AnimatedCursor from "../component/cursor1";
+import AnimatedCursor from "react-animated-cursor";
 
 export default function myApp({ Component, pageprops }) {
   return (
     <div className="App">
-      <AnimatedCursor />
+      <AnimatedCursor
+        color="255,255,255"
+        innerSize={8}
+        outerSize={35}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={1}
+        hasBlendMode={true}
+        outerStyle={{
+          mixBlendMode: "exclusion",
+        }}
+        innerStyle={{
+          backgroundColor: "#FFF",
+          mixBlendMode: "exclusion",
+        }}
+        clickables={[
+          "a, p, h1, h2, h3, h4, h5, h6, span",
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          "select",
+          "textarea",
+          "button",
+          ".link",
+        ]}
+      />
       <Component {...pageprops} />
     </div>
   );
